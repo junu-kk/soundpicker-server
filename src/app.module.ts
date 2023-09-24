8afcfa8ee71642f1397a8f0b9657419d96ae7c10
Kang Junewoo <brianjune.dev@gmail.com>
2023-09-24 22:15:03 +0900

    [PR] Add DB schema (#10)
    * Add mongoose packages
    
    * Import MongooseModule
    
    * Add schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserModule } from './user/user.module'
+import { TestModule } from './test/test.module'
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule,
+    TestModule,
+  ], // import ConfigModule first to use dotenv
   controllers: [AppController],
   providers: [AppService],
 })
