34c25565a6b6929c27320dae4ec7f7d1337abc3b
junu-kk <brianjune.dev@gmail.com>
2023-09-24 21:34:41 +0900

    Import MongooseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { UsersService } from './users/users.service'
+import { TestsService } from './tests/tests.service'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.DB_URI)], // import ConfigModule first to use dotenv
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService, TestsService],
 })
 export class AppModule {}
