0e5f775cac89095502447cf2529b1d8324ba4826
junu-kk <brianjune.dev@gmail.com>
2023-09-30 00:12:22 +0900

    Remove patch api

--- src/tests/tests.controller.ts
+++ src/tests/tests.controller.ts
@@ -1,16 +1,7 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { TestsService } from './tests.service'
 import { GetTestsQueryDto } from './dto/getTests.dto'
 import { ParseObjectIdPipe } from '../common/pipes/parseObjectId.pipe'
-import { UpdateTestDto } from './dto/updateTest.dto'
 import { CreateTestDto } from './dto/createTest.dto'
 
 @Controller('tests')
@@ -30,13 +21,5 @@ export class TestsController {
   @Post('/')
   async createTest(@Body() body: CreateTestDto) {
     return { body }
-  }
-
-  @Patch('/:testId')
-  async updateTest(
-    @Param('testId', ParseObjectIdPipe) testId: string,
-    @Body() body: UpdateTestDto,
-  ) {
-    return { testId, body }
   }
 }
