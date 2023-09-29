03ac87ae60ef8f661821beb23888ae1036ed3909
junu-kk <brianjune.dev@gmail.com>
2023-09-29 23:44:31 +0900

    Make all tests pass

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthService } from './auth.service'
+import { UsersService } from '../users/users.service'
+import { getModelToken } from '@nestjs/mongoose'
+import { User } from '../users/schemas/user.schema'
+import { Model } from 'mongoose'
+import { JwtService } from '@nestjs/jwt'
 
 describe('AuthService', () => {
   let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UsersService,
+        JwtService,
+        {
+          provide: getModelToken(User.name),
+          useValue: Model<User>,
+        },
+      ],
     }).compile()
 
     service = module.get<AuthService>(AuthService)
--- src/tests/tests.service.spec.ts
+++ src/tests/tests.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { TestsService } from './tests.service'
+import { getModelToken } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 
 describe('TestsService', () => {
   let service: TestsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TestsService],
+      providers: [
+        TestsService,
+        { provide: getModelToken(Test.name), useValue: Model<Test> },
+      ],
     }).compile()
 
     service = module.get<TestsService>(TestsService)
