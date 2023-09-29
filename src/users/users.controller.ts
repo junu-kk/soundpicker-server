b8943487188862a72d9174565acdcfddf19121cf
junu-kk <brianjune.dev@gmail.com>
2023-09-30 00:47:40 +0900

    Delete getUser() and pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Param, Post } from '@nestjs/common'
 import { UsersService } from './users.service'
-import { ParseObjectIdPipe } from '../common/pipes/parseObjectId.pipe'
 import { CreateTokenDto } from './dto/createToken.dto'
 import { CreateUserBodyDto } from './dto/createUser.dto'
 import { Public } from '../auth/public.decorator'
@@ -15,15 +14,10 @@ export class UsersController {
     return this.usersService.createUser(body)
   }
 
-  @Get('/:userId')
-  async getUser(@Param('userId', ParseObjectIdPipe) userId: string) {
-    return { userId }
-  }
-
   @Public()
   @Post('/:userId/tokens')
   async createToken(
-    @Param('userId', ParseObjectIdPipe) userId: string,
+    @Param('userId') userId: string,
     @Body() body: CreateTokenDto,
   ) {
     return { userId, body }
